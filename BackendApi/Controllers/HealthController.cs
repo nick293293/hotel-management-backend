65c95da274af7c6793589832853c6eb1735fd630
Nick <karenartyom2016@gmail.com>
2025-10-15 13:25:50 -0700

    added db health controller to test database heatlh

---
+++ BackendApi/Controllers/HealthController.cs
@@ -0,0 +1,27 @@
+﻿using BackendApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public HealthController(AppDbContext db) => _db = db;
+
+        [HttpGet("db")]
+        public async Task<IActionResult> Db()
+        {
+            try
+            {
+                var can = await _db.Database.CanConnectAsync();
+                return can ? Ok(new { ok = true }) : StatusCode(500, new { ok = false });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { ok = false, error = ex.Message });
+            }
+        }
+    }
+}

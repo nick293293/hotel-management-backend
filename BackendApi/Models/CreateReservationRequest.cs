46e2466345694f004221bd61f551ba452b20379f
Nick <karenartyom2016@gmail.com>
2025-11-18 16:48:53 -0800

    updates to post request for reservation

--- BackendApi/Models/CreateReservationRequest.cs
+++ BackendApi/Models/CreateReservationRequest.cs
@@ -1,12 +1,14 @@
-﻿namespace BackendApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendApi.Models
 {
     public class CreateReservationRequest
     {
-        public int UserId { get; set; }
-        public int RoomId { get; set; }
-        public DateTime CheckInDate { get; set; }
-        public DateTime CheckOutDate { get; set; }
-        public decimal TotalAmount { get; set; }
-        public string? Status { get; set; }  // optional, defaults to "reserved"
+        [Required] public int UserId { get; set; }
+        [Required] public int RoomId { get; set; }
+        [Required] public DateOnly CheckInDate { get; set; }
+        [Required] public DateOnly CheckOutDate { get; set; }
+        [Range(0, double.MaxValue)] public decimal TotalAmount { get; set; }
+        public string? Status { get; set; }
     }
 }
---
+++ BackendApi/Utilities/DateOnlyJsonConverter.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public sealed class DateOnlyJsonConverter : JsonConverter<DateOnly>
+{
+    private const string Format = "yyyy-MM-dd";
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        => DateOnly.Parse(reader.GetString()!);
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+        => writer.WriteStringValue(value.ToString(Format));
+}
